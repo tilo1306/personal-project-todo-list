30b3e995098c604e31efd37e003b5e488f4b808b
Douglas dos Santos Oliveira <doug1306@gmail.com>
2022-07-07 21:04:14 -0300

    feat: config app and server.ts

---
+++ todo-list/src/app.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from 'express'
+import cors from 'cors'
+import path from 'path'
+
+const server = express()
+server.use(cors())
+server.use(express.static(path.join(__dirname, '../public')))
+server.use(express.urlencoded({ extended: true }))
+server.use(express.json())
+
+server.use((req: Request, res: Response) => {
+  res.status(404).json({ error: 'Endpoint nâo Encontrado' })
+})
+
+export default server
---
+++ todo-list/src/server.ts
@@ -0,0 +1,5 @@
+import app from './app'
+
+app.listen(3001, async () => {
+  console.log('Rodando o servidor')
+})
