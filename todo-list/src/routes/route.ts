265e0fff451f01036566930927238791f8b39cbb
Douglas dos Santos Oliveira <doug1306@gmail.com>
2022-07-07 21:12:13 -0300

    feat: create routes

---
+++ todo-list/src/routes/route.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express'
+
+const router = Router()
+
+router.post('/register')
+router.post('/login')
+router.get('/task/:id')
+router.post('/task/:id')
+router.put('/task/:id')
+router.delete('task/:id')
