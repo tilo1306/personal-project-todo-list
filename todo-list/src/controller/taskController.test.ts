4621879c70fafa91a2eb1b9425f76f538cdf63be
Douglas dos Santos Oliveira <doug1306@gmail.com>
2022-07-10 22:20:08 -0300

    test: testing login controller

--- todo-list/src/controller/taskController.test.ts
+++ todo-list/src/controller/taskController.test.ts
@@ -1,6 +1,7 @@
 import { prisma } from '../database/prisma'
 import request from 'supertest'
 import app from '../app'
+import { Itypes } from '../types/typeModel'
 
 describe('Testing Api route register', () => {
   const email = 'tiloWolf@gmail.com'
@@ -8,5 +9,22 @@ describe('Testing Api route register', () => {
   beforeAll(async () => {
     await prisma.tasks.deleteMany({ where: {} })
     await prisma.users.deleteMany({ where: {} })
+    await prisma.users.create({
+      data: {
+        email, password
+      }
+    })
+  })
+  afterAll(async () => {
+    await prisma.tasks.deleteMany({ where: {} })
+    await prisma.users.deleteMany({ where: {} })
+    await prisma.$disconnect()
+  })
+  it('testing register router', async () => {
+    const user = await prisma.users.findFirst({
+      where: { email }
+    }) as Itypes
+    const res = await request(app).post(`/task/${user.id}`)
+    expect(res.statusCode).toEqual(200)
   })
 })
