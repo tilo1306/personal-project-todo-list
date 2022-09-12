3eb6041d8049a4428c56b2e670eb414029533f86
Douglas dos Santos Oliveira <doug1306@gmail.com>
2022-09-11 21:52:16 -0300

    feat: task screen

--- todo-list/mobile/src/screen/Tasks/index.tsx
+++ todo-list/mobile/src/screen/Tasks/index.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import { Text } from 'react-native';
-import { Container } from './styled';
+import {
+  Container,
+  AreaTask,
+  AreaTitle,
+  Title,
+  Logo,
+  InputAdd,
+} from './styled';
 export const Tasks: React.FC = () => {
   return (
     <Container>
-      <Text>Todo-List</Text>
+      <AreaTask>
+        <AreaTitle>
+          <Title>Todo List</Title>
+          <Logo />
+        </AreaTitle>
+        <InputAdd />
+        <Text>Todo-List</Text>
+      </AreaTask>
     </Container>
   );
 };
--- todo-list/mobile/src/screen/Tasks/styled.ts
+++ todo-list/mobile/src/screen/Tasks/styled.ts
@@ -1,6 +1,49 @@
 import styled from 'styled-components/native';
+import { RFValue } from 'react-native-responsive-fontsize';
+import Logo1 from '../../assets/logo.svg';
+import { Input } from '../../components/Inputs';
 
 export const Container = styled.View`
-  flex: 1;
   background-color: ${({ theme }) => theme.color.primary};
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  padding: ${RFValue(30)}px;
 `;
+
+export const AreaTask = styled.View`
+  width: 100%;
+  min-height: ${RFValue(620)}px;
+  background-color: ${({ theme }) => theme.color.secondary};
+  border-radius: ${RFValue(10)}px;
+  align-items: center;
+  padding: ${RFValue(36)}px ${RFValue(20)}px;
+`;
+
+export const AreaTitle = styled.View`
+  flex-direction: row;
+`;
+
+export const Title = styled.Text`
+  color: ${({ theme }) => theme.color.white};
+  font-family: ${({ theme }) => theme.font.forum};
+  font-size: ${RFValue(40)}px;
+  margin-right: ${RFValue(5)}px;
+`;
+
+export const Logo = styled(Logo1)`
+  fill: ${({ theme }) => theme.color.white};
+  width: ${RFValue(38)}px;
+  height: ${RFValue(38)}px;
+`;
+export const InputAdd = styled(Input)`
+  background-color: #f6f7f8;
+  border-radius: ${RFValue(5)}px;
+  margin-top: ${RFValue(50)}px;
+  margin-bottom: ${RFValue(26)}px;
+  height: ${RFValue(30)}px;
+  width: 100%;
+`;
+
+export const ViewTask = styled.View``;
+export const TextTask = styled.Text``;
